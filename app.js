03c85324055bc922d551d15ad54c47c25a17b28c
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-06-13 23:02:22 +0700

    update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ sequelize
   .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
-    return sequelize.sync();
+    //return sequelize.sync();
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
