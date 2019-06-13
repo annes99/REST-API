3a0c4f75730e93bf8145c94b3deaf9acc13d33ff
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-06-13 22:09:13 +0700

    change validation

--- db/models/User.js
+++ db/models/User.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// id (Integer, primary key, auto-generated)
-// firstName (String)
-// lastName (String)
-// emailAddress (String)
-// password (String)
-
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         id: {
@@ -16,50 +10,23 @@ module.exports = (sequelize, DataTypes) => {
         firstName: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                // will only allow letters
-                is: ["^[a-z]+$",'i'],
-                // don't allow empty strings
-                notEmpty: {
-                    msg: "First name is required"
-                } 
-            }
         },
         lastName: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                // will only allow letters
-                is: ["^[a-z]+$",'i'],
-                // don't allow empty strings
-                notEmpty: {
-                    msg: "Last name is required"
-                } 
-            }
         },
         emailAddress: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                // checks for email format (foo@bar.com)
-                isEmail: true,
-                // don't allow empty strings
-                notEmpty: {
-                    msg: "Must be valid email address"
-                } 
-            }
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
-            notEmpty: {
-                msg: "Enter password"
-            } 
         },
     });
 
     User.associate = (models) => {
-        User.hasMany(models.Course);
+        User.hasMany(models.Course, { foreignKey: 'userId' });
     };
 
     return User;
