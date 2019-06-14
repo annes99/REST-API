624bebcca747a008c918d2ee151969780f821753
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-06-14 20:19:45 +0700

    fix course.update

--- routes/routes.js
+++ routes/routes.js
@@ -35,22 +35,22 @@ router.get('/users', authenticateUser, asyncMiddleware(async (req, res) => {
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 router.post('/users', validate('user'), asyncMiddleware(async (req, res) => {
 
-  // Attempt to get the validation result from the Request object.
+  // Attempt to get the validation result from the Request object
   const errors = validationResult(req);
 
-  // If there are validation errors...
+  // If there are validation errors
   if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
+    // Use the Array `map()` method to get a list of error messages
     const errorMessages = errors.array().map(error => error.msg);
 
-    // Return the validation errors to the client.
+    // Return the validation errors to the client
     return res.status(400).json({ errors: errorMessages });
   }
 
-  // Get the user from the request body.
+  // Get the user from the request body
   const user = req.body;
 
-  //Check if email address isn't already associated with an existing user
+  // Check if email address isn't already associated with an existing user
   const findUser = await User.findOne({
     where: {
       emailAddress: user.emailAddress
@@ -73,7 +73,7 @@ router.post('/users', validate('user'), asyncMiddleware(async (req, res) => {
     return res.status(201).location('/').end();
 
   } else {
-    // If user with provided email address already exists, set status 400 Bad Request with error message.
+    // If user with provided email address already exists, set status 400 Bad Request with error message
     return res.status(400).json({ message: `User with email address: ${user.emailAddress} already exists` });
   }
 
@@ -85,12 +85,13 @@ router.post('/users', validate('user'), asyncMiddleware(async (req, res) => {
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
 router.get('/courses', asyncMiddleware(async (req, res) => {
   const courses = await Course.findAll({
+    attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
     include: [{
       model: User,
       attributes: ['firstName', 'lastName', 'emailAddress']
     }]
   });
-
+  // Set response status code 200 OK and show course
   return res.status(200).json({ courses });
 
 }));
@@ -98,11 +99,12 @@ router.get('/courses', asyncMiddleware(async (req, res) => {
 
 // GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
 router.get('/courses/:id', asyncMiddleware(async (req, res) => {
-  // find course by ID
+  // Find course by ID
   const course = await Course.findOne({
     where: {
       id: req.params.id
     },
+    attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
     include: [{
       model: User,
       attributes: ['firstName', 'lastName', 'emailAddress']
@@ -111,6 +113,7 @@ router.get('/courses/:id', asyncMiddleware(async (req, res) => {
   });
 
   if (course) {
+    // Set response status code 200 OK and show course
     return res.status(200).json({ course });
   } else {
     // If no course with provided ID is found set status 404 Not Found
@@ -119,25 +122,25 @@ router.get('/courses/:id', asyncMiddleware(async (req, res) => {
 
 }));
 
-// POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
+// POST /api/courses 201 - Creates a course, 
+// Set the Location header to the URI for the course, and return no content
 router.post('/courses', validate('course'), authenticateUser, asyncMiddleware(async (req, res) => {
-  // Attempt to get the validation result from the Request object.
+  // Attempt to get the validation result from the Request object
   const errors = validationResult(req);
 
-  // If there are validation errors...
+  // If there are validation errors
   if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
+    // Use the Array `map()` method to get a list of error messages
     const errorMessages = errors.array().map(error => error.msg);
 
-    // Return the validation errors to the client.
+    // Return the validation errors to the client
     return res.status(400).json({ errors: errorMessages });
 
   } else {
 
-    // Get the course from the request body.
+    // Get the course from the request body and create new course
     const course = await req.body;
 
-    // Creating new course
     const newCourse = Course.create({
       title: course.title,
       description: course.description,
@@ -145,25 +148,25 @@ router.post('/courses', validate('course'), authenticateUser, asyncMiddleware(as
       materialsNeeded: course.materialsNeeded,
       userId: req.currentUser.id
     });
-    // Set status 201 Created, the Location header to the URI for the course and end the response.
+    // Set status 201 Created, the Location header to the URI for the course and end the response
     return res.status(201).location(`api/courses/${newCourse.id}`).end();
   }
 }));
 
 
 // PUT /api/courses/:id 204 - Updates a course and returns no content
-// route returns a 403 status code if the current user doesn't own the requested course
+// Route returns a 403 status code if the current user doesn't own the requested course
 router.put('/courses/:id', validate('course'), authenticateUser, asyncMiddleware(async (req, res) => {
   // Attempt to get the validation result from the Request object.
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
+    // Use the Array `map()` method to get a list of error messages
     const errorMessages = errors.array().map(error => error.msg);
-    // Return the validation errors to the client.
+    // Return the validation errors to the client
     return res.status(400).json({ errors: errorMessages });
   } else {
-    // find course by ID
+    // Find course by ID
     const course = await Course.findOne({
       where: {
         id: req.params.id
@@ -175,30 +178,31 @@ router.put('/courses/:id', validate('course'), authenticateUser, asyncMiddleware
 
     });
 
-    // if course exists
+    // If course exists
     if (course) {
-      // if course owner matches current user
+      // If course owner ID matches current user ID update course
       if (course.userId === req.currentUser.id) {
-        // update course details in Courses table
-        course.update();
+        course.update(req.body);
+        // Set status 204 No Content and end the response
         return res.status(204).end();
 
       } else {
-        // Return a response with a 403 Client forbidden HTTP status code.
+        // Set response status code 403 Forbidden
         return res.status(403).json({ message: 'Access Forbidden' });
       }
     } else {
+      // Set response status code 404 Not Found 
       return res.status(404).json({ message: 'Page not found' });
     }
   }
 }));
 
 // DELETE /api/courses/:id 204 - Deletes a course and returns no content
-// route returns a 403 status code if the current user doesn't own the requested course
+// Route returns a 403 status code if the current user doesn't own the requested course
 
 router.delete('/courses/:id', authenticateUser, asyncMiddleware(async (req, res) => {
 
-  // find course by ID
+  // Find course by ID
   const course = await Course.findOne({
     where: {
       id: req.params.id
@@ -209,19 +213,20 @@ router.delete('/courses/:id', authenticateUser, asyncMiddleware(async (req, res)
     }]
 
   });
-  // if course with given ID exists
+  // If course with given ID exists
   if (course) {
-    // if course owner ID matches current user ID
+    // If course owner ID matches current user ID delete course
     if (course.userId === req.currentUser.id) {
-      // delete course
       course.destroy();
+      // Set status 204 No Content and end the response
       return res.status(204).end();
 
     } else {
-      // Return a response with a 403 Client forbidden HTTP status code.
+      // Set response status code 403 Forbidden
       return res.status(403).json({ message: 'Access Forbidden' });
     }
   } else {
+    // Set response status code 404 Not Found 
     return res.status(404).json({ message: 'Page not found' });
   }
 }));
